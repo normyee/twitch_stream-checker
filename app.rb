975a69d448f612553f810a6268d2660af18fdbdf
normyee <lunoqro@gmail.com>
2023-10-06 10:30:19 -0300

    refactor: instantiating the StreamChecker class

--- app.rb
+++ app.rb
@@ -1,37 +1,11 @@
-require "launchy"
-require "colorize"
-require 'rest-client'
-require "json"
-require_relative "lib/config/config.rb"
-require_relative "lib/utils/messageInfo"
+require_relative "./lib/StreamChecker/StreamChecker.rb"
 
 puts "What's your streamer name: ".green
+
 streamer = gets.chomp
-stream_url = "https://www.twitch.tv/#{streamer}"
+streamChecker = StreamChecker.new(streamer)
 
-url = "https://api.twitch.tv/helix/streams?user_login=#{streamer}"
+streamChecker.execute
 
-response = RestClient.get(url, {
-    "Authorization": "Bearer " + Config.get_access_token,
-    "Client-Id": Config.client_id
-})
-
-
-if (response.code >= 200 && response.code < 300 )
-    data = JSON.parse(response)["data"][0]
-    if (!data)
-        puts "The streamer #{streamer} is offline".red
-        return
-    end
-    messageInfo(streamer, data["game_name"], data["title"], data["viewer_count"])
-    Launchy.open(stream_url)
-    return
-
-end
-
-if (response.code >= 400 && response.code < 500)
-    puts "Error: couldn't check streamer :(".red
-    return
-end
 
 
